8d2fe3a7ebe5296999ef6a89ab1058e7dff5fe2b
Bruno Oliveira <58921482+brunoliveiradev@users.noreply.github.com>
2024-01-04 22:00:38 -0300

    [FEATURE]: Create a proxy to call ViaCEP API - get method

---
+++ packages/busca-cep/domain/cep.go
@@ -0,0 +1,13 @@
+package domain
+
+type ViaCEP struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Unidade     string `json:"unidade"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+}
---
+++ packages/busca-cep/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/brunoliveiradev/GoExpertPostGrad/packages/busca-cep/domain"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"regexp"
+	"time"
+)
+
+const (
+	getPath   = "/busca"
+	getKey    = "cep"
+	localPort = ":8080"
+)
+
+var httpClient = &http.Client{Timeout: 10 * time.Second}
+
+func main() {
+	http.HandleFunc(getPath, BuscaCepHandler)
+	http.ListenAndServe(localPort, nil)
+}
+
+func BuscaCepHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	if r.URL.Path != getPath {
+		http.Error(w, "Page not found", http.StatusNotFound)
+		return
+	}
+
+	cepParam := r.URL.Query().Get(getKey)
+	if cepParam == "" {
+		http.Error(w, "CEP is required", http.StatusBadRequest)
+		return
+	}
+
+	matched, _ := regexp.MatchString(`^\d{8}$`, cepParam)
+	if !matched {
+		http.Error(w, "Invalid CEP format", http.StatusBadRequest)
+		return
+	}
+
+	cep, err := BuscaCep(cepParam)
+	if err != nil {
+		log.Printf("Error fetching CEP: %v", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	result, err := json.Marshal(cep)
+	if err != nil {
+		log.Printf("Error encoding CEP: %v", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+	w.Write(result)
+}
+
+func BuscaCep(cep string) (*domain.ViaCEP, error) {
+	resp, err := httpClient.Get("https://viacep.com.br/ws/" + cep + "/json/")
+	if err != nil {
+		var netErr net.Error
+		ok := errors.As(err, &netErr)
+		if ok && netErr.Timeout() {
+			log.Printf("Request timed out: %v", err)
+			return nil, fmt.Errorf("request timed out")
+		}
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var viaCEP domain.ViaCEP
+	err = json.Unmarshal(body, &viaCEP)
+	if err != nil {
+		return nil, err
+	}
+
+	return &viaCEP, nil
+}

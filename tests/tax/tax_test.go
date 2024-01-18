160579b9ec99f2d24eca203f85205908e3638db5
Bruno Oliveira <58921482+brunoliveiradev@users.noreply.github.com>
2024-01-18 20:56:59 -0300

    [FEATURE]: Test in batches

--- tests/tax/tax_test.go
+++ tests/tax/tax_test.go
@@ -13,3 +13,24 @@ func TestCalculateTax(t *testing.T) {
 		t.Errorf("Expected tax of %v for amount of %v, but got %v", expected, amount, tax)
 	}
 }
+
+func TestCalculateTaxBatch(t *testing.T) {
+	type calcTax struct {
+		amount   float64
+		expected float64
+	}
+
+	table := []calcTax{
+		{1000.0, 100.0},
+		{500.0, 25.0},
+		{200.0, 10.0},
+		{1200.0, 120.0},
+	}
+
+	for _, test := range table {
+		tax := CalculateTax(test.amount)
+		if tax != test.expected {
+			t.Errorf("Expected tax of %v for amount of %v, but got %v", test.expected, test.amount, tax)
+		}
+	}
+}

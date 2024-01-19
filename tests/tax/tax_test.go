ae93009afdfe701d519223ce5dc4d1be6f13b108
Bruno Oliveira <58921482+brunoliveiradev@users.noreply.github.com>
2024-01-18 21:07:26 -0300

    [FEATURE]: Benchmarking Example

--- tests/tax/tax_test.go
+++ tests/tax/tax_test.go
@@ -34,3 +34,16 @@ func TestCalculateTaxBatch(t *testing.T) {
 		}
 	}
 }
+
+func BenchmarkCalculateTax(b *testing.B) {
+	amount := 1000.0
+	expected := 100.0
+
+	for i := 0; i < b.N; i++ {
+		tax := CalculateTax(amount)
+
+		if tax != expected {
+			b.Errorf("Expected tax of %v for amount of %v, but got %v", expected, amount, tax)
+		}
+	}
+}

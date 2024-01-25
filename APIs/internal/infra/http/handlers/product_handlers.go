6f29092b46598169ecc517c54f823451da83792c
Bruno Oliveira <58921482+brunoliveiradev@users.noreply.github.com>
2024-01-25 16:35:02 -0300

    [FEATURE]: Create UpdateProduct Handler

--- APIs/internal/infra/http/handlers/product_handlers.go
+++ APIs/internal/infra/http/handlers/product_handlers.go
@@ -6,7 +6,9 @@ import (
 	"github.com/brunoliveiradev/courseGoExpert/APIs/internal/domain"
 	"github.com/brunoliveiradev/courseGoExpert/APIs/internal/dto"
 	"github.com/brunoliveiradev/courseGoExpert/APIs/internal/infra/database"
+	"github.com/brunoliveiradev/courseGoExpert/APIs/pkg/entity"
 	"github.com/go-chi/chi"
+	"log"
 	"net/http"
 )
 
@@ -20,20 +22,24 @@ func NewProductHandler(db database.ProductInterface) *ProductHandler {
 
 func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
 	var input dto.CreateProductInput
+
 	err := json.NewDecoder(r.Body).Decode(&input)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
+		log.Printf("Error decoding request body: %v", err)
+		http.Error(w, "Bad request", http.StatusBadRequest)
 		return
 	}
 
 	product, err := domain.NewProduct(input.Name, input.Price)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
+		log.Printf("Error creating new product: %v", err)
+		http.Error(w, "Bad request", http.StatusBadRequest)
 		return
 	}
 	err = h.ProductDB.Create(product)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("Error creating product in database: %v", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
 		return
 	}
 	w.WriteHeader(http.StatusCreated)
@@ -45,17 +51,56 @@ func (h *ProductHandler) GetProduct(w http.ResponseWriter, r *http.Request) {
 	product, err := h.ProductDB.FindByID(id)
 	if err != nil {
 		if errors.Is(err, database.ErrProductNotFound) {
-			w.WriteHeader(http.StatusNotFound)
+			log.Printf("Product not found: %v", err)
+			http.Error(w, "Not found", http.StatusNotFound)
 			return
 		}
-		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("Error getting product from database: %v", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(product); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("Error encoding product to JSON: %v", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
 		return
 	}
+}
+
+func (h *ProductHandler) UpdateProduct(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+
+	var inputProduct domain.Product
+	err := json.NewDecoder(r.Body).Decode(&inputProduct)
+	if err != nil {
+		log.Printf("Error decoding request body: %v", err)
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+
+	inputProduct.ID, err = entity.ParseID(id)
+	if err != nil {
+		log.Printf("Error parsing ID: %v", err)
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+
+	err = h.ProductDB.Update(&inputProduct)
+	if err != nil {
+		if errors.Is(err, database.ErrProductNotFound) {
+			log.Printf("Product not found: %v", err)
+			http.Error(w, "Not found", http.StatusNotFound)
+			return
+		}
+		log.Printf("Error updating product in database: %v", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
 }
--- APIs/pkg/entity/id.go
+++ APIs/pkg/entity/id.go
@@ -5,10 +5,10 @@ import "github.com/google/uuid"
 type ID = uuid.UUID
 
 func NewID() ID {
-	return ID(uuid.New())
+	return uuid.New()
 }
 
 func ParseID(id string) (ID, error) {
 	newId, err := uuid.Parse(id)
-	return ID(newId), err
+	return newId, err
 }
